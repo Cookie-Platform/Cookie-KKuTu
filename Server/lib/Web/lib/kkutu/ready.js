c2ba4f59b11c925f52edbccb999ca4d50ac263b8
cookedas1 <mulmando1991@gmail.com>
2025-04-19 15:18:59 +0900

    확장자 변경

--- Server/lib/Web/lib/kkutu/ready.js
+++ Server/lib/Web/lib/kkutu/ready.js
@@ -705,7 +705,7 @@ $(document).ready(function(){
 		var fileName = "KKuTu" + (
 			date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " "
 			+ date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds()
-		) + ".kkt";
+		) + ".ckikkt";
 		var $a = $("<a>").attr({
 			'download': fileName,
 			'href': url
